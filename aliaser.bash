90119dfa061b9c33ddead273811a48dbb45c9d09
unforswearing <support@unforswearing.com>
2017-09-27 14:46:26 -0400

    attempting to use https://github.com/gko/listbox for searching

--- aliaser.bash
+++ aliaser.bash
@@ -20,6 +20,133 @@ be sure to source the alias file in your .bashrc or .bash_profile
 EOF
 }
 
+# https://github.com/gko/listbox
+listbox() {
+  while [[ $# -gt 0 ]]
+  do
+    key="$1"
+
+    case $key in
+      -h|--help)
+        echo "choose from list of options"
+        echo "Usage: listbox [options]"
+        echo "Example:"
+        echo "  listbox -t title -o \"option 1|option 2|option 3\" -r resultVariable -a '>'"
+        echo "Options:"
+        echo "  -h, --help                         help"
+        echo "  -t, --title                        list title"
+        echo "  -o, --options \"option 1|option 2\"  listbox options"
+        echo "  -r, --result <var>                 result variable"
+        echo "  -a, --arrow <symbol>               selected option symbol"
+        return 0
+        ;;
+      -o|--options)
+        local OIFS=$IFS;
+        IFS="|";
+        # check if zsh/bash
+        if [ -n "$ZSH_VERSION" ]; then
+          IFS=$'\n' opts=($(echo "$2" | tr "|" "\n"))
+        else
+          IFS="|" read -a opts <<< "$2";
+        fi
+        IFS=$OIFS;
+        shift
+        ;;
+      -t|--title)
+        local title="$2"
+        shift
+        ;;
+      -r|--result)
+        local __result="$2"
+        shift
+        ;;
+      -a|--arrow)
+        local arrow="$2"
+        shift
+        ;;
+      *)
+    esac
+    shift
+  done
+
+  if [[ -z $arrow ]]; then
+    arrow=">"
+  fi
+
+  local len=${#opts[@]}
+
+  local choice=0
+  local titleLen=${#title}
+
+  if [[ -n "$title" ]]; then
+    echo -e "\n  $title"
+    printf "  "
+    printf %"$titleLen"s | tr " " "-"
+    echo ""
+  fi
+
+  draw() {
+    local idx=0 
+    for it in "${opts[@]}"
+    do
+      local str="";
+      if [ $idx -eq $choice ]; then
+        str+="$arrow "
+      else
+        str+="  "
+      fi
+      echo "$str$it"
+      idx=$((idx+1))
+    done
+  }
+
+  move() {
+    for it in "${opts[@]}"
+    do
+      tput cuu1
+    done
+    tput el1
+  }
+
+  listen() {
+    while true
+    do
+      key=$(bash -c "read -n 1 -s key; echo \$key")
+
+      if [[ $key = q ]]; then
+        break
+      elif [[ $key = B ]]; then
+        if [ $choice -lt $((len-1)) ]; then
+          choice=$((choice+1))
+          move
+          draw
+        fi
+      elif [[ $key = A ]]; then
+        if [ $choice -gt 0 ]; then
+          choice=$((choice-1))
+          move
+          draw
+        fi
+      elif [[ $key = "" ]]; then
+        # check if zsh/bash
+        if [ -n "$ZSH_VERSION" ]; then
+          choice=$((choice+1))
+        fi
+
+        if [[ -n $__result ]]; then
+          eval "$__result=\"${opts[$choice]}\""
+        else
+          echo -e "\n${opts[$choice]}"
+        fi
+        break
+      fi
+    done
+  }
+
+  draw
+  listen
+}
+
 # error checking, not implemented.
 _test_() {
 	local illegal
@@ -36,6 +163,7 @@ _test_() {
 
 config=""${HOME}"/.aliaser/aliaser.conf"
 
+# Beginnings of a 'back' command. need to figure out directory tracking (is it worth it?)
 # previous=""${HOME}"/.aliaser/previous.dir"
 # pwd > "$previous"
 
@@ -92,11 +220,39 @@ _named() {
 }
 
 _search() {
+	shopt -s expand_aliases
+
 	if [[ "$2" == "" ]]; then
 		printf "aliaser search needs an alias name to search for";
 	else
-        echo "Search results for "$2""
-        grep -i "$2" "$file"
+# 		results() { 
+# 			grep -i "$2" "$file" | \
+# 			sed "s/\#.*//g;s/'//g;s/cd //g" | \
+# 			grep -v '^$' | awk -F '=' '{print $2}' | \
+# 			tr '\n' '\|'
+# 		}
+#       aliasname=$(echo "$dir" | awk -F '=' '{print $1}' | awk -F ' ' '{print $2}')
+	
+		results() { 
+			grep -i "$2" "$file" | sed "s/\#.*//g" | grep -v '^$' | tr '\n' '\|'
+		}
+
+		choosefromlist() {
+			listbox -t "Search results for "\"$2\""" -o "$(results "$@")" -r comm
+			toexec=$(echo "$comm" | awk -F '=' '{print $2}' | sed "s/'//g")
+			trigger=$(echo $toexec | awk -F ' ' '{print $1}')
+			directory=$(echo $toexec | sed 's/cd //g;s/^/"/g;s/$/"/g')
+
+			if [[ $trigger == "cd" ]]; then 
+				# this doesn't wanna work
+				eval pushd "$directory" 
+			else 
+				eval "$toexec"
+			fi
+		}
+
+		echo "Search results for "\"$2\"":"
+		grep -i "$2" "$file"
     fi
 }
 
@@ -109,7 +265,6 @@ _command() {
 
 # create tmp alias: aliaser tmp "docs" dir
 # remove tmp alias: aliser tmp rm "docs"
-# allow returning to previous dir: aliaser back
 
 case "$1" in
 	-h|--help|help|"") helpp; . "$file";;

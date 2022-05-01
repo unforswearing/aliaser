a5384ebd224622532776901b108a750b0102eccb
unforswearing <al.charity@gmail.com>
2022-05-01 17:06:49 -0400

    work on aliaser.lua

---
+++ aliases.lua
@@ -0,0 +1,7 @@
+{
+  aliaser = "cd /Users/unforswearing/Documents/__Github/aliaser",
+  lsprojects = "ls /Users/unforswearing/Documents/Scripts/Projects",
+  proj_aliaser = "cd /Users/unforswearing/Documents/__Github/aliaser",
+  projects = "cd /Users/unforswearing/Documents/Scripts/Projects",
+  testington = "echo command"
+}

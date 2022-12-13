66908a0d79a3d6b9c3a07af3e2c707ebef63da95
Alvin <al.charity@gmail.com>
2022-12-13 16:35:22 -0500

    rm testing

--- aliases.lua
+++
@@ -1,7 +0,0 @@
-{
-  aliaser = "cd /Users/unforswearing/Documents/__Github/aliaser",
-  lsprojects = "ls /Users/unforswearing/Documents/Scripts/Projects",
-  proj_aliaser = "cd /Users/unforswearing/Documents/__Github/aliaser",
-  projects = "cd /Users/unforswearing/Documents/Scripts/Projects",
-  testington = "echo command"
-}

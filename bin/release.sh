060342505dec4478709512a33728a6468865f223
Alvin <al.charity@gmail.com>
2025-04-03 18:24:53 -0400

    add release.sh to update tags

---
+++ bin/release.sh
@@ -0,0 +1,13 @@
+# usage: bash release.bash $(update) "release message"
+function release() {
+  local version="$1"
+  local message="$2"
+  if [[ -z "$version" ]]; then 
+    echo "$0: no version to tag this release."
+    echo "usage: release <version> <message>"
+    return 1
+  fi
+  git tag -a "v${version}" -m "${message}"
+  git push origin "v${version}"
+  npm publish
+}
